17c1975c3298689ac1713c894cd7a9d2f6e00d79
WANG Tianyi <twang006@e.ntu.edu.sg>
2015-12-21 08:40:11 +0000

    TW: Engine timer finished
    git-tfs-id: [https://twang006.visualstudio.com/]$/TW;C131

--- TW/TW_Engine/StopWatch.h
+++ TW/TW_Engine/StopWatch.h
@@ -7,35 +7,36 @@ namespace TW
 {
 namespace Timing
 {
-	class __declspec(dllexport) StopWatch
-	{
-	public:
 
-		StopWatch();
-		~StopWatch();
+class __declspec(dllexport) StopWatch
+{
+public:
+	StopWatch();
+	~StopWatch();
 
-		void start();				//! Start timer
-		void stop();				//! Stop timer
-		void reset();				//! Reset timer
+	void start();				//! Start timer
+	void stop();				//! Stop timer
+	void reset();				//! Reset timer
 
-		//! Get elapsed time after calling start() or time between
-		//! stop() and start()
-		double getElapsedTime();	
+	//! Get elapsed time after calling start() or time between
+	//! stop() and start()
+	double getElapsedTime();	
 
-		//! Average time = TotalTime / number of stops
-		double getAverageTime();
+	//! Average time = TotalTime / number of stops
+	double getAverageTime();
 	
-	private:
-		LARGE_INTEGER m_startTime;	//! Start time
-		LARGE_INTEGER m_endTime;	//! End time
+private:
+	LARGE_INTEGER m_startTime;	//! Start time
+	LARGE_INTEGER m_endTime;	//! End time
 
-		double m_dElapsedTime;		//! Time elapsed between the last start and stop
-		double m_dTotalTime;			//! Time elapsed between all starts and stops
-		bool m_isRunning;			//! Judge if timer is still running
-		int m_iNumClockSessions;	//! Number of starts and stops
-		double m_dFreq;				//! Frequency
-		bool m_isFreqSet;			//! Judge if the frequency is set 
-	};
+	double m_dElapsedTime;		//! Time elapsed between the last start and stop
+	double m_dTotalTime;			//! Time elapsed between all starts and stops
+	bool m_isRunning;			//! Judge if timer is still running
+	int m_iNumClockSessions;	//! Number of starts and stops
+	double m_dFreq;				//! Frequency
+	bool m_isFreqSet;			//! Judge if the frequency is set 
+};
+
 }	// namespace Timing
 }	// namespace TW
 
--- TW/TW_EngineTester/ClockTests.cpp
+++ TW/TW_EngineTester/ClockTests.cpp
@@ -1,49 +1,37 @@
 #include <gtest\gtest.h>
-#include "Clock.h"
+#include "StopWatch.h"
 #include <QTest>
 
 using namespace TW::Timing;
 
-TEST(CClock, Initialize)
+TEST(StopWatch, FrameTimeMeasuing)
 {
-	CClock clock;
-	EXPECT_TRUE(clock.initialize());
-	EXPECT_TRUE(clock.shutdown());
-}
+	StopWatch clock;
 
-TEST(CClock, FrameTimeMeasuing)
-{
-	CClock clock;
-
-	EXPECT_TRUE(clock.initialize());
+	clock.start();
 	QTest::qSleep(1000);
-	clock.newFrame();
-	float timedTime = clock.timeElapsedLastFrame();
+	float timedTime = clock.getElapsedTime() / 1000;
 	EXPECT_TRUE(0.9f < timedTime);
 	EXPECT_TRUE(timedTime < 1.1f);
-	clock.newFrame();
+	clock.reset();
 	QTest::qSleep(500);
-	clock.newFrame();
-	timedTime = clock.timeElapsedLastFrame();
+	timedTime = clock.getElapsedTime() / 1000;
 	EXPECT_TRUE(0.4f < timedTime);
 	EXPECT_TRUE(timedTime < 0.6f);
 
-	const int NUM_TESTS = 10 + rand() % 100;
+	clock.reset();
+
+	const int NUM_TESTS = 1 + rand() % 10;
 	const float THRESHOLD = 0.1f;
 
 	for (int i = 0; i < NUM_TESTS; i++)
 	{
 		int thisTestTimeMilliseconds = rand() % 10000;
 		float thisTestTimeSeconds = thisTestTimeMilliseconds / 1000.0f;
-		clock.newFrame();
+		clock.reset();
 		QTest::qSleep(thisTestTimeMilliseconds);
-		clock.newFrame();
-		float elapsedSeconds = clock.timeElapsedLastFrame();
+		float elapsedSeconds = clock.getElapsedTime() / 1000;
 		EXPECT_TRUE((thisTestTimeSeconds - THRESHOLD) < elapsedSeconds);
-		EXPECT_TRUE(elapsedSeconds < (thisTestTimeSeconds - THRESHOLD));
+		EXPECT_TRUE(elapsedSeconds < (thisTestTimeSeconds + THRESHOLD));
 	}
-	
-	clock.newFrame();
-	clock.timeElapsedLastFrame();
-	EXPECT_TRUE(clock.shutdown());
 }
