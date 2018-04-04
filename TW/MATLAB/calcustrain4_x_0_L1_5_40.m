4bb916d2b6d60aa378e72f20c3f80813b2c0c0de
TWANG006 <twang006@e.ntu.edu.sg>
2018-04-04 11:18:57 +0800

    non-uniform experiments added

--- TW/MATLAB/calcustrain4_x_0_L1_5_40.m
+++ TW/MATLAB/calcustrain4_x_0_L1_5_40.m
@@ -3,18 +3,19 @@ close all
 clear
 clc
 %y=load('E:\em one 20171227\data\raw_fig4\24def_0_8_n2_FFTCC-ICGN_BC_O8_Data.csv');
-y=load('E:\paper\em two 20180106\data\raw_data_displace4\5L1_FFTCC-ICGN_BC_O8_Data.csv');
+y=load('Sample 14 L3 Amp0_8.3_Data.csv');
 v8=y(:,4);%
 %[h1,w1]=size(v8);
-h2=103;
-w2=395;
+h2=34;
+w2=326;
 v88=reshape(v8,w2,h2);
 figure;
-surf(v88')
-M=16;%strain window 2*M+1
+surf(v88'); colormap(jet);
+shading interp;
+M=9;%strain window 2*M+1
 GS=5;%grid space
-SuSi=20;%subset size SuSi*2+1
-MarSi=10;% margin space
+SuSi=10;%subset size SuSi*2+1
+MarSi=200;% margin space
 %uvalue=0.000000006;
 accuracyorder=8;% accuracy order O8
 %coe1=-1*M:M;
@@ -30,8 +31,15 @@ for i=M+1:w2-M
         st1(i-M,j-M)=3/(GS*M*(M+1)*(2*M+1)^2)*st0*10^(0);
     end
 end
+
+st1 = st1*10^6;
 figure;
-surf(st1')
+surf(st1'); colormap(jet);
+shading interp;
+xlabel('X (pixels)');
+ylabel('Y (pixels)');
+zlabel('Calculated Strain (με)');
+
 maxy=max(max(st1))
 miny=min(min(st1))
 %u4=u(:,4);%加载位移数据。
@@ -39,8 +47,10 @@ miny=min(min(st1))
 %u44=u4;%需要对应改动大小。
 %=u44;
 
-y=load('E:\paper\em two 20180106\pattern\Sample14\Sample 14 Commanded Displacement.csv');
-v8=y(:,3);
+st1one = st1';
+
+y=load('Sample 14 Commanded Displacement.csv');
+v8=y(:,5);
 
 [h,w]=size(st1);
 weiz=1:h;
@@ -53,8 +63,20 @@ jzvalue=st1;
 for jj=1:w
     jzvalue(:,jj)=hvalue;
 end
-jzvalue1=jzvalue*10^(-6);
-figure;surf(jzvalue1')
+jzvalue1=jzvalue;
+figure;surf(jzvalue1'); colormap(jet);
+shading interp;
+xlabel('X (pixels)');
+ylabel('Y (pixels)');
+zlabel('Pre-set Strain (με)');
+
+jzvalue1one = jzvalue1';
+figure; plot(jzvalue1one(1,:)); hold on
+plot(st1one(1,:));
+xlabel('X (pixels)');
+ylabel('Strain (με)');
+legend('Pre-set Strain','Calculated Strain');
+
 st2=st1-jzvalue1;%
 syst=mean2(st2)%
 std2=std2(st1)%除以n-1
